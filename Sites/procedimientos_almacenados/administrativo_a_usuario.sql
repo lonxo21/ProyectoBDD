c0b5a938f66a8965789bf981dea2ec300a6c4551
lonxo21 <alonso.venegas@uc.cl>
2021-07-05 17:58:41 -0400

    proceso almacenado v4

--- Sites/procedimientos_almacenados/administrativo_a_usuario.sql
+++ Sites/procedimientos_almacenados/administrativo_a_usuario.sql
@@ -5,10 +5,17 @@ mover_administrativos_dblink()
 RETURNS void AS $$
 
 
-DECLARE @counter int;
-DECLARE tupla_adm RECORD ;
+DECLARE idmax int;
+        idmax2 int;
+        tupla_adm RECORD ;
 
-SET @counter = 1
+SELECT INTO idmax
+max(usuario.id)
+FROM usuario
+
+SELECT INTO idmax2
+max(direccion.id)
+FROM direccion
 
 BEGIN
     FOR tupla_adm IN (SELECT * FROM
@@ -21,13 +28,14 @@ BEGIN
     LOOP
         IF NOT EXISTS (select usuario.rut from usuario where usuario.rut = tupla_adm.rut)
         BEGIN
-            INSERT INTO Usuario VALUES(364 + tupla_adm.id, tupla_adm.nombre, tupla_adm.rut, tupla_adm.edad, tupla_adm.sexo)
+            INSERT INTO Usuario VALUES(idmax + 1, tupla_adm.nombre, tupla_adm.rut, tupla_adm.edad, tupla_adm.sexo);
             IF NOT EXISTS (select direccion.nombre from direccion where direccion.nombre = tupla_adm.direccion)
             BEGIN
-                INSERT INTO Direccion VALUES(1097 + @counter, direccion, comuna)
+                INSERT INTO Direccion VALUES(idmax2 + 1, direccion, comuna);
+                SET @counter = @counter + 1
             END
         END 
-        SET @counter = @counter + 1
+
 
     END LOOP;
 
