38015481fcb52e62107046f0666bcc5bea28c71b
DanoUC <dealegria@uc.cl>
2021-07-07 11:12:16 -0400

    ahora si .sql

--- Sites/procedimientos_almacenados/producto_en_tienda.sql
+++ Sites/procedimientos_almacenados/producto_en_tienda.sql
@@ -6,7 +6,7 @@ RETURNS integer AS $$
 
 BEGIN
 
-IF EXISTS (select producto.id from productos, stock, tienda where productos.id = stock.id_producto and tienda.id = sotck.id_tienda) THEN
+IF EXISTS (select productos.id from productos, stock, tienda where productos.id = stock.id_producto and tienda.id = sotck.id_tienda) THEN
     RETURN 1;
 
 ELSE
