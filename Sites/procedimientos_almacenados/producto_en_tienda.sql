a0d7c1c9b1ccaf577c1a3f8eaabad623ef3138b2
DanoUC <dealegria@uc.cl>
2021-07-07 11:08:40 -0400

    producto en tienda?.sql

---
+++ Sites/procedimientos_almacenados/producto_en_tienda.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION
+
+producto_en_tienda(idproducto int, idtienda int)
+
+RETURNS integer AS $$
+
+BEGIN
+
+IF EXISTS (select producto.id from productos, stock, tienda where productos.id = stock.id_producto and tienda.id = sotck.id_tienda) THEN
+    RETURN 1;
+
+ELSE
+    RETURN 0;
+
+END IF;
+
+
+END;
+
+
+
+$$ language plpgsql
